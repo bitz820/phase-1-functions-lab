44c12dbdcd762d556bbd157259f7ff7891520415
Mark <mark.n.bacon.jr@gmail.com>
2022-03-14 14:03:41 -0400

    commit tree 6d73b68eacc6f60ad01ddb77be8c637a60520b60

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(block) {
+    if (block < 42) {
+        return (block - 42) * -1
+    } else
+        return block - 42
+}
+
+function distanceFromHqInFeet(num) {
+    return distanceFromHqInBlocks(num) * 264
+}
+
+function distanceTravelledInFeet(start, end) {
+    if (end < start) {
+        return (start - end) * 264
+    }
+    else { return (end - start) * 264 }
+}
+
+function calculatesFarePrice(start, end) {
+    let distance = distanceTravelledInFeet(start, end)
+    if (distance < 400) {
+        return 0
+    } else if (distance > 400 && distance < 2000) {
+        return (distance-400)*.02
+    } else if (distance > 2000 && distance < 2500) {
+        return 25
+    }else if (distance > 2500){
+        return 'cannot travel that far'
+    }
+}
